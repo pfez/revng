2c2f7abd80a0ba445609094e5f4701666e6a19bd
Pietro Fezzardi <pietro@rev.ng>
2024-11-28 12:54:39 +0100

    fixup-generator-iterator

--- include/revng/ADT/GeneratorIterator.h
+++ include/revng/ADT/GeneratorIterator.h
@@ -5,6 +5,7 @@
 //
 
 #include <cstddef>
+#include <optional>
 #include <type_traits>
 
 #include "revng/Support/Debug.h"
@@ -17,6 +18,9 @@ concept TriviallyCopyable = std::is_trivially_copyable_v<T>
                             and std::is_copy_constructible_v<T>
                             and std::is_copy_assignable_v<T>;
 
+// Iterator class that generates the next element on increment, holding a
+// cppcoro::generator.
+//
 // We restrict the `T` type so that is a trivially copyable object
 template<TriviallyCopyable T>
 class GeneratorIterator {
@@ -24,133 +28,105 @@ public:
   // We need the specification of the following `iterator_traits` so that
   // `llvm::GraphWriter` doesn't complain in trying to perform `std::distance`
   // over the `child_begin`/`child_end` traits
-  using difference_type = std::ptrdiff_t;
+  using difference_type = ssize_t;
   using value_type = T;
   using pointer = T *;
   using reference = T &;
   using iterator_category = typename std::forward_iterator_tag;
 
-public:
-  using inner_iterator = cppcoro::generator<T>::iterator;
+  using inner_iterator = decltype(cppcoro::generator<T>().begin());
 
-public:
-  bool IsEnd = false;
-  mutable bool IsDead = false;
-  mutable cppcoro::generator<T> Coroutine;
+private:
+  mutable cppcoro::generator<T> Generator;
   inner_iterator Begin;
-  inner_iterator End;
 
-private:
-  // The `SnapshotContent` `std::optional` field is used when constructing a
-  // snapshotted iterator, i.e., a special iterator state which can only be
-  // dereferenced (this is used in order to provide a post-increment operator,
-  // which would need to perform a copy of the main iterator object, operation
-  // that would violate the unicity of the ownership of the `Coroutine` object)
-  std::optional<T> SnapshotContent;
+  // Optional field is used when constructing a snapshotted iterator, i.e. a
+  // special iterator state which can only be dereferenced.
+  mutable std::optional<T> Snapshot;
 
 public:
-  // Constructor for building a end sentinel iterator
-  GeneratorIterator() : IsEnd(true) {}
+  // Constructor building an empty coroutine, with no snapshots.
+  // This is basically a coroutine that is already ended, representing an empty
+  // range.
+  GeneratorIterator() : Generator(), Begin(), Snapshot(std::nullopt) {}
 
-  // Standard constructor providing an input `Coroutine`, which is used during
-  // the increment of the iterator
-  GeneratorIterator(cppcoro::generator<T> &&Coroutine) :
-    IsEnd(false),
-    Coroutine(std::move(Coroutine)),
-    Begin(this->Coroutine.begin()),
-    End(this->Coroutine.end()) {}
+  // Standard constructor providing an input generator, that will be used under
+  // the hood to generate the iterators.
+  GeneratorIterator(cppcoro::generator<T> &&TheGenerator) :
+    Generator(std::move(TheGenerator)),
+    Begin(Generator.begin()),
+    Snapshot(std::nullopt) {}
+
+  ~GeneratorIterator() = default;
 
 private:
-  // Constructor used for a snapshotted iterator, which should only be invoked
-  // in the post increment, so this is private
-  GeneratorIterator(T &Snapshot) : SnapshotContent(Snapshot) {}
+  // Constructor used for a snapshotted iterator.
+  // This constructor is private since it's only invoked in the postincrement
+  // operator, and doesn't really make sense as public API.
+  GeneratorIterator(const T &Value) : Generator(), Begin(), Snapshot(Value) {}
 
 public:
-  // Copy constructor which uses the copy assignment
-  GeneratorIterator(const GeneratorIterator &Other) { *this = Other; }
-
-  // Copy assignment. The semantics of this is non-trivial and appear counter
-  // intuitive. We are constrained by the existence of the private `Coroutine`
-  // field, which owns the state of the iteration progression, and that cannot
-  // be copied. However, due to the behavior of the llvm `iterator_range` class,
-  // we need to perform a copy of the iterator, a copy in which only the LHS of
-  // the result survives (the RHS is an immediately discarded temporary).
-  // Therefore we implement a copy constructor that takes into consideration
-  // this behavior, and which leaves the RHS in a `IsDead` state, which is
-  // asserted as first thing in any method of the `GeneratorIterator`, in order
-  // to ensure that it is never used.
+  // Copy assignment.
   GeneratorIterator &operator=(const GeneratorIterator &Other) {
-    IsEnd = Other.IsEnd;
-    Other.IsDead = true;
-    Coroutine = std::move(Other.Coroutine);
-    Begin = Other.Begin;
-    End = Other.End;
+    if (this != &Other) {
+      this->Snapshot = Other.Snapshot;
+      // We move the coroutine, even if we're in the copy constructor, because
+      // the coroutine is not copyable.
+      // In principle we wouldn't like to define this copy constructor at all,
+      // but there's come code in LLVM we depend upon that requires it and that
+      // we don't control.
+      // So we have to make sure that the coroutine is never copied and is in
+      // fact moved.
+      this->Generator = std::move(Other.Generator);
+      this->Begin = std::move(Other.Begin);
+    }
     return *this;
   }
 
-  // We rely on the default copy construct and assignment
+  // Copy constructor which uses the copy assignment
+  GeneratorIterator(const GeneratorIterator &Other) { *this = Other; }
+
+  // Move assignment
+  GeneratorIterator &operator=(GeneratorIterator &&Other) {
+    if (this != &Other) {
+      this->Snapshot = std::move(Other.Snapshot);
+      this->Generator = std::move(Other.Generator);
+      this->Begin = std::move(Other.Begin);
+    }
+    return *this;
+  }
+
+  // Move constructor which uses the move assignment
+  GeneratorIterator(GeneratorIterator &Other) { *this = std::move(Other); }
+
+public:
+  bool isSnapshot() const { return Snapshot.has_value(); }
 
 public:
   bool operator==(const GeneratorIterator &Other) const {
-    // We should not invoke any operation on an iterator instance left in the
-    // `IsDead` state by a copy operation
-    revng_assert(not IsDead);
-    revng_assert(not Other.IsDead);
+    // It's not legal to compare iterators in snapshot state.
+    revng_assert(not this->isSnapshot() and not Other.isSnapshot());
 
-    // Verify that we are not in the `Snapshot` state, before attempting a
-    // comparison
-    revng_assert(not SnapshotContent.has_value());
-
-    // In the comparison operator, we need to explicitly handle the sentinel
-    // iterator state, both for the situation where `this` is a sentinel
-    // iterator, and when `Other` is a sentinel iterator. If both the iterator
-    // objects are not sentinel, we compare the internal iterators
-    if (IsEnd)
-      return Other.Begin == Other.End;
-    if (Other.IsEnd)
-      return Begin == End;
-    else
-      return Begin == Other.Begin;
+    return this->Begin == Other.Begin;
   }
 
   GeneratorIterator &operator++() {
-    // We should not invoke any operation on an iterator instance left in the
-    // `IsDead` state by a copy operation
-    revng_assert(not IsDead);
-
-    // Verify that we are not in the `Snapshot` state
-    revng_assert(not SnapshotContent.has_value());
-
+    // It's not legal to increment an iterator in snapshot state.
+    revng_assert(not isSnapshot());
     ++Begin;
     return *this;
   }
 
   GeneratorIterator operator++(int) {
-    // We should not invoke any operation on an iterator instance left in the
-    // `IsDead` state by a copy operation
-    revng_assert(not IsDead);
+    // It's not legal to increment an iterator in snapshot state.
+    revng_assert(not isSnapshot());
 
-    // Verify that we are not in the `Snapshot` state
-    revng_assert(not SnapshotContent.has_value());
-
-    // We return an iterator in the `Snapshot` state, so it can be dereferenced
-    // and T obtained, as required by the contract of the postincrement operator
-    // for a forward iterator, but we do not perform a full copy that would
-    // create problems due to the uniqueness of the internal `Coroutine`
-    GeneratorIterator Ret(*Begin);
+    // We return an iterator in the snapshot state, that can only be
+    // dereferenced to obtain the snapshotted value.
+    GeneratorIterator TakenSnapshot(*Begin);
     ++*this;
-    return Ret;
+    return TakenSnapshot;
   }
 
-  T operator*() const {
-    // We should not invoke any operation on an iterator instance left in the
-    // `IsDead` state by a copy operation
-    revng_assert(not IsDead);
-
-    if (SnapshotContent.has_value()) {
-      return *SnapshotContent;
-    }
-
-    return *Begin;
-  }
+  T operator*() const { return Snapshot.value_or(*Begin); }
 };
