395a08cdca1c6047a3d7f9730d4b168ec69f8a78
Pietro Fezzardi <pietro@rev.ng>
2026-01-15 14:27:08 +0100

    Segregate: add "revng.pointers" metadata
    This commit extends SegregateStackAccesses to ensure it attaches the
    "revng.pointers" metadata to all call sites and llvm::Functions it
    rewrites.
    
    This metadata is used to represent in LLVM IR, in a way that is
    model-agnostic, the information about what arguments and return values
    (both of calls or functions, so actual and formal) are pointer-typed
    in the model.
    
    This information is consumed by later passes, that in this way are
    allowed to be model-agnostic as well.

--- lib/PromoteStackPointer/SegregateStackAccessesPass.cpp
+++ lib/PromoteStackPointer/SegregateStackAccessesPass.cpp
@@ -285,6 +285,28 @@ makeVariableBuilder(const model::Binary &Binary,
   }
 }
 
+struct PointersMetadata {
+  llvm::SmallVector<bool> ReturnValues;
+  llvm::SmallVector<bool> Arguments;
+};
+
+static PointersMetadata getPointerMetadata(const abi::FunctionType::Layout &L) {
+  PointersMetadata Result;
+
+  if (L.hasSPTAR()) {
+    // SPTAR always returns a pointer on LLVM IR.
+    Result.ReturnValues.push_back(true);
+  } else {
+    for (const auto &R : L.ReturnValues)
+      Result.ReturnValues.push_back(R.Type->isPointer());
+  }
+
+  for (const auto &R : L.Arguments)
+    Result.Arguments.push_back(R.Type->isPointer());
+
+  return Result;
+}
+
 /// Rewrite all stack memory accesses
 ///
 /// This pass changes the base address of stack memory access to either:
@@ -1169,6 +1191,8 @@ private:
     CallInst *NewCall = B.CreateCall(CalleeType, CalledValue, Arguments);
     NewCall->copyMetadata(*OldCall);
     NewCall->setAttributes(OldCall->getAttributes());
+    const auto &[PointerReturns, PointerArguments] = getPointerMetadata(Layout);
+    setPointersMetadata(NewCall, PointerReturns, PointerArguments);
 
     switch (Layout.returnMethod()) {
     case ReturnMethod::ModelAggregate: {
@@ -1463,6 +1487,8 @@ private:
 
     // Create the new function, stealing the name
     Function &NewFunction = recreateWithoutBody(*OldFunction, NewType);
+    const auto &[PointerReturns, PointerArguments] = getPointerMetadata(Layout);
+    setPointersMetadata(&NewFunction, PointerReturns, PointerArguments);
 
     // Record the old-to-new mapping
     OldToNew[OldFunction] = &NewFunction;
