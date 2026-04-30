697365793a3c1f773f430a713f5de362aeba94a4
Ivan Krysak <ivan@rev.ng>
2026-04-30 11:11:42 +0000

    CliftEmitC: expose `emitCommonIncludes`

--- lib/CliftEmitC/Headers.cpp
+++ lib/CliftEmitC/Headers.cpp
@@ -236,6 +236,12 @@ public:
   }
 };
 
+void emitCommonIncludes(ptml::CTokenEmitter &Tokens,
+                        const CDataModel &DataModel) {
+  CHeaderEmitterImpl Emitter(Tokens, DataModel, TypeEmitterConfiguration{});
+  Emitter.emitCommonIncludes();
+}
+
 void emitTypeAndGlobalHeader(ptml::CTokenEmitter &Tokens,
                              mlir::ModuleOp Module,
                              TypeEmitterConfiguration Configuration,
