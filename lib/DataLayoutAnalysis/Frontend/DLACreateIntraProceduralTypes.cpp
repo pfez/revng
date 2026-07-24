fbf6ac9cf1538fc93d8842c7488a86155ed9b918
Pietro Fezzardi <pietro@rev.ng>
2026-07-24 10:39:38 +0200

    tmp-fix-comments-to-backport

--- lib/DataLayoutAnalysis/Frontend/DLACreateIntraProceduralTypes.cpp
+++ lib/DataLayoutAnalysis/Frontend/DLACreateIntraProceduralTypes.cpp
@@ -267,8 +267,17 @@ public:
         // Add entry in SCEVToLayoutType map for values returned by F
         if (auto *RetI = dyn_cast<ReturnInst>(&I)) {
           if (Value *RetVal = RetI->getReturnValue()) {
-            // An array-of-bytes return value is an ABI-lowered aggregate whose
-            // type is already known in the model, so it gets no layout type.
+            // TODO: An array-of-bytes return value for a CABI function
+            // returning an aggregate. In order to handle it properly we should
+            // visit it recursively and have first-class support for model types
+            // in DLA, which we currently don't have.
+            // So, for now we just bail out, accepting to degrade the quality of
+            // the results. We don't expect this to affect users much in
+            // practice, because either the input has no debug symbols (and in
+            // that case DLA sees mostly Raw functions, and this case doesn't
+            // trigger) or the model (via debug symbols, or user input) has lots
+            // of good information on CABI function types, in which case the
+            // role of DLA is not that important interprocedurally.
             if (isArrayOfBytes(RetVal->getType()))
               continue;
             revng_assert(isa<StructType>(RetVal->getType())
