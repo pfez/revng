59c0eb4adbb23036c8a4ffeb09e2ccf77a8700ff
Pietro Fezzardi <pietro@rev.ng>
2026-07-24 10:39:50 +0200

    tmp-fix-comments-to-backport

--- lib/DataLayoutAnalysis/Frontend/DLACreateIntraProceduralTypes.cpp
+++ lib/DataLayoutAnalysis/Frontend/DLACreateIntraProceduralTypes.cpp
@@ -526,9 +526,17 @@ public:
               }
             }
           } else if (isArrayOfBytes(C->getType())) {
-            // An array-of-bytes call result is an ABI-lowered aggregate whose
-            // type is already known in the model; skip it. Its size is still
-            // captured through the alloca it is stored into.
+            // TODO: An array-of-bytes call to a CABI function returning an
+            // aggregate. In order to handle it properly we should visit it
+            // recursively and have first-class support for model types in DLA,
+            // which we currently don't have.
+            // So, for now we just bail out, accepting to degrade the quality of
+            // the results. We don't expect this to affect users much in
+            // practice, because either the input has no debug symbols (and in
+            // that case DLA sees mostly Raw functions, and this case doesn't
+            // trigger) or the model (via debug symbols, or user input) has lots
+            // of good information on CABI function types, in which case the
+            // role of DLA is not that important interprocedurally.
           } else {
             // Type representing the return type
             revng_assert(not C->getType()->isIntegerTy(1));
